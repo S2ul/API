88759383ae0f31ee5fba4584a47952a3c5f6fed8
S2.log <83286420+S2ul@users.noreply.github.com>
2023-08-21 16:42:51 +0900

    Add files via upload

---
+++ api json/Items.java
@@ -0,0 +1,6 @@
+package com.peisia.spring.mi.vo.kw; 
+import java.util.ArrayList;
+import java.util.List; 
+public class Items{
+    public ArrayList<Item> item;
+}
---
+++ api json/KWeatherVo.java
@@ -0,0 +1,32 @@
+package com.peisia.spring.mi.vo.kw;
+
+import lombok.Data;
+
+@Data
+public class KWeatherVo {
+    public Response response;
+
+    @Data
+    public static class Response {
+        public Body body;
+
+        @Data
+        public static class Body {
+            public String dataType;
+            public Items items;
+
+            @Data
+            public static class Items {
+                public Item item;
+
+                @Data
+                public static class Item {
+                    public String stnNm;
+                    public String minTa;
+                    public String maxTa;
+                    // 다른 속성들
+                }
+            }
+        }
+    }
+}
