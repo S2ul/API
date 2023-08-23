404ee38fbf15d216d70a75ab75723f360a23d3b3
S2.log <83286420+S2ul@users.noreply.github.com>
2023-08-23 15:08:01 +0900

    Add files via upload

---
+++ resources/common.js
@@ -0,0 +1,13 @@
+
+// 함수 정의: 로깅 기능
+function log(message) {
+    console.log(message);
+}
+
+// 변수 정의: 기본 애니메이션 속도
+var defaultAnimationSpeed = 1000;
+
+// jQuery 문서 로딩 후 실행
+$(function() {
+    // 여기에 공통적으로 사용되는 코드가 있다면 작성
+});
